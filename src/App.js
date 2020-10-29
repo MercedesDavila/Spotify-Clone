3b6be8de6d4d728357942a00ab8e116721e0f1ca
MercedesDavila <mmdavila@outlook.com>
2020-10-29 17:39:25 -0600

    Conexión a spotify auth con token

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Player from "./Player";
+import { getTokenFromUrl } from "./spotify";
+import "./App.css";
+import Login from "./Login";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi ();
+
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello! Sppotify Clone App 🚀 </h1>
 
-      {/*Spotify Logo */}
-      {/* Login with spotify button */}
-      
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    // Set token
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if(_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user =>{
+      console.log('🙋 ' , user);
+      })
+    }
+
+
+    console.log("I HAVE A TOKEN ", _token);
+  }, []);
+
+  return (
+    <div className="app">
+      {
+      token ? 
+      <Player />
+       : 
+      <Login />
+      }
     </div>
   );
 }
---
+++ src/Player.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Player.css";
+
+
+function Player({ spotify }) {
+  return (
+    <div className="player">
+<h1>Welcome to Spotify</h1>
+    </div>
+  );
+}
+
+export default Player;
